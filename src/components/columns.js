a6695c8d84089e830ca49523fb072284b2930cd2
unknown <kyleward1992@gmail.com>
2021-01-30 22:03:21 -0500

    columns.js

---
+++ src/components/columns.js
@@ -0,0 +1,30 @@
+import { ColumnFilter } from "./ColumnFilter";
+
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        accessor: 'id',
+        Filter: ColumnFilter,
+    },
+    {
+        Header: 'Name',
+        accessor: 'name',
+        Filter: ColumnFilter,
+    },
+    {
+        Header: 'Email',
+        accessor: 'email',
+        Filter: ColumnFilter,
+    },
+    {
+        Header: 'Gender',
+        accessor: 'gender',
+        Filter: ColumnFilter,
+    },
+    {
+        Header: 'Age',
+        accessor: 'age',
+        Filter: ColumnFilter,
+    }
+
+]
