9515b8374994806d5660f5ee3ccebb3b73a7ced1
unknown <kyleward1992@gmail.com>
2021-01-30 22:11:33 -0500

    app.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css'
+import { SortingTable } from './components/SortingTable'
+
+function App() {
+    return (
+        <div className='App'>
+            <SortingTable />
+        </div>
+    )
+}
+
+export default App;
